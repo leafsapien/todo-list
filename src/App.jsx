5061b4c7a605759ea603454cb0563d0188531b8a
Anaya <anaya.basak@gmail.com>
2024-11-22 21:33:31 +1000

    Build basic todo list functionality including text, task creation, task completion, some minor styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import TodoItem from './components/TodoItem';
+import TodoForm from './components/TodoForm';
+import './App.css'
+
 function App() {
-  return <h1>My todo list</h1>;
+
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false,
+    },{
+      text: "Meet friend for lunch",
+      isCompleted: false,
+    },{
+      text: "Build really cool todo app",
+      isCompleted: false,
+    }
+  ]);
+
+  const addTodo = (text) => {
+      const newTodos = [...todos, { text }];
+      setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+      const newTodos = [...todos];
+      newTodos[index].isCompleted = true;
+      setTodos(newTodos);
+  };
+
+return (
+      <div className='app'>
+          <h1 className='todo-list'>My todo list</h1>
+          {todos.map((todo, index) => (
+              <TodoItem todo={todo} key={index} index={index} completeTodo={completeTodo} />
+          ))}
+          <TodoForm addTodo={addTodo} />
+      </div>
+  );
 }
 
 export default App;
